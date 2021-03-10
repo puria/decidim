7b72d1c13952a7f2dafbd99ae48e3ffc09cd5455
Vera Rojman <vrojman@protonmail.com>
2021-03-10 15:24:11 +0100

    Use first photo attachment as card image for blog posts
    # Conflicts:
    #	decidim-blogs/spec/system/explore_posts_spec.rb

--- decidim-blogs/spec/system/explore_posts_spec.rb
+++ decidim-blogs/spec/system/explore_posts_spec.rb
@@ -9,12 +9,19 @@ describe "Explore posts", type: :system do
   let!(:old_post) { create(:post, component: component, created_at: Time.current - 2.days) }
   let!(:new_post) { create(:post, component: component, created_at: Time.current) }
 
+  let!(:image) { create(:attachment, attached_to: old_post) }
+
   describe "index" do
     it "shows all posts for the given process" do
       visit_component
       expect(page).to have_selector(".card", count: 2)
       expect(page).to have_selector(".card--post", text: translated(new_post.title))
       expect(page).to have_selector(".card--post", text: translated(old_post.title))
+    end
+
+    it "shows images" do
+      visit_component
+      expect(page).to have_selector(".card--post img.card__image")
     end
 
     context "when paginating" do
